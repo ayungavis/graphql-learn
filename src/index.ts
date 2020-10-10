5f4fb29d17bd79bcaab3ca1a31fb2000fe9d5fd4
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-10-10 15:20:46 +0700

    Add CRUD mutation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 const { GraphQLServer } = require("graphql-yoga");
 
 let links = [
@@ -15,14 +17,33 @@ const resolvers = {
     feed: () => links,
   },
   Mutation: {
-    post: (parent, args) => {
+    createPost: (parent, args) => {
       const link = {
         id: `link-${idCount++}`,
         description: args.description,
         url: args.url,
       };
+
       links.push(link);
       return link;
+    },
+    updatePost: (parent, args) => {
+      const index = links.findIndex((item) => item.id === args.id);
+      const item = links.find((item) => item.id === args.id);
+      const result = {
+        ...item,
+        url: args.url,
+        description: args.description,
+      };
+
+      links.splice(index, 1);
+      links.push(result);
+      return result;
+    },
+    deletePost: (parent, args) => {
+      const index = links.findIndex((item) => item.id === args.id);
+      links.splice(index, 1);
+      return `Link successfully deleted!`;
     },
   },
   Link: {
