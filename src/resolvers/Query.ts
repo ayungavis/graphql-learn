3cdc79740cc5d084f8467c5e9a0f51149a15c377
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-10-18 23:16:22 +0700

    Add filter, sort, pagination, and counter

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,21 @@
-export function feed(parent, args, context, info) {
-  return context.prisma.link.findMany();
+export async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return { links, count };
 }
